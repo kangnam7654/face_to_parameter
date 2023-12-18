bb9dfaa6c31698c8c7bd92787de80df978598c60
Kangnam Kim <kangnam7654@naver.com>
2023-12-18 11:41:32 +0900

    for save

---
+++ utils/move_samples.py
@@ -0,0 +1,12 @@
+import argparse
+from pathlib import Path
+
+def get_args():
+    pass
+
+def main(args):
+    directory = Path(args.root_dir)
+    print("EHHLP")
+    
+if __name__ == "__main__":
+    main()
